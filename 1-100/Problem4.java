98f72df107cfb3df7853c1c159b7917489faf798
Blake1412 <blakeryan30@gmail.com>
2021-12-20 16:12:39 +0000

    Create Problem4.java

---
+++ 1-100/Problem4.java
@@ -0,0 +1,21 @@
+public class Problem4 {
+    public static int answer() {
+        int largest = 0;
+        for (int i = 100; i < 1000; i++) {
+            for (int j = i; j < 1000; j++) {
+                int flag = 0;
+                String number = String.valueOf(i * j);
+                for (int k = 0; k < number.length(); k++) {
+                    if (number.charAt(k) != number.charAt(number.length() - 1 - k)) {
+                        flag++;
+                        break;
+                    }
+                }
+                if (flag == 0 && i * j > largest) {
+                    largest = (i * j);
+                }
+            }
+        }
+        return largest;
+    }
+}
