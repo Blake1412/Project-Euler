7aab6e9d949fed0976e014a2ba6c870ef83e8fe3
Blake1412 <blakeryan30@gmail.com>
2021-12-20 23:05:49 +0000

    Create Problem7.java

---
+++ 1-100/Problem7.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+
+public class Problem7 {
+    public static int answer() {
+        int number = 0, count = 1;
+        for (int i = 3; i > 0; i += 2) {
+            int flag = 0;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    flag++;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                count++;
+            }
+            if (count == 10001) {
+                number = i;
+                break;
+            }
+        }
+        return number;
+    }
+}
