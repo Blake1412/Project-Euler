7695ac3ba3b04d8eda8e338da0da17f4b15ea3cc
Blake1412 <blakeryan30@gmail.com>
2021-12-20 20:46:50 +0000

    Create Problem6.java

---
+++ 1-100/Problem6.java
@@ -0,0 +1,10 @@
+public class Problem6 {
+    public static int answer() {
+        int sum = 0;
+        int square =  (int) Math.pow((100 * 101 / 2), 2);
+        for (int i = 1; i <= 100; i++) {
+            sum += Math.pow(i, 2);
+        }
+        return Math.abs(square - sum);
+    }
+}
