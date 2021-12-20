2058dd2cf77f5601e30439ca54c5c15bd6a06304
Blake1412 <blakeryan30@gmail.com>
2021-12-20 20:31:43 +0000

    Create Problem5.java

---
+++ 1-100/Problem5.java
@@ -0,0 +1,19 @@
+public class Problem5 {
+    public static int answer() {
+        int number = 0;
+        for (int i = 20; i >= 0; i += 20) {
+            int flag = 0;
+            for (int j = 1; j <= 20; j++) {
+                if (i % j != 0) {
+                    flag++;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                number = i;
+                break;
+            }
+        }
+        return number;
+    }
+}
