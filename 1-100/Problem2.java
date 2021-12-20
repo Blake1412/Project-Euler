ef2087f28549f605a90d284709efda1f4bb30947
Blake1412 <blakeryan30@gmail.com>
2021-12-20 15:07:44 +0000

    Create Problem2.java

---
+++ 1-100/Problem2.java
@@ -0,0 +1,15 @@
+public class Problem2 {
+    public static int answer() {
+        int a = 1, b = 1, c = 0;
+        int sum = 0;
+        while (c < 4000000) {
+            c = a + b;
+            a = b;
+            b = c;
+            if (c % 2 == 0) {
+                sum += c;
+            }
+        }
+        return sum;
+    }
+}
