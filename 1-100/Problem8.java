dfb5a7d9e216d7984124e2f7b68018eeadac0b33
Blake1412 <blakeryan30@gmail.com>
2021-12-20 23:36:44 +0000

    Create Problem8.java

---
+++ 1-100/Problem8.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Problem8 {
+    public static long answer() throws FileNotFoundException {
+        String number = "";
+        Scanner scan = new Scanner(new File("data.txt"));
+        while (scan.hasNextLine()) {
+            number += scan.nextLine();
+        }
+
+        long largestProduct = 0;
+        for (int i = 0; i < number.length()/2 - 12; i++) {
+            long product = 1;
+            for (int j = 0; j <= 12; j++) {
+                product *= Character.getNumericValue(number.charAt((i + j)));
+            }
+            if (product > largestProduct) {
+                largestProduct = product;
+            }
+        }
+        return largestProduct;
+    }
+}
