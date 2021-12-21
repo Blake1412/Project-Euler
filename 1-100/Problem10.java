8eed933bfaeef6d2cccc819ef61732f280cc15f6
Blake1412 <blakeryan30@gmail.com>
2021-12-21 14:33:59 +0000

    Update Problem10.java

--- 1-100/Problem10.java
+++ 1-100/Problem10.java
@@ -1,7 +1,7 @@
 public class Problem10 {
     public static long answer() {
         long sum = 2;
-        for (int i = 3; i < 2000000; i ++) {
+        for (int i = 3; i < 2000000; i += 2) {
             int flag = 0;
             for (int j = 2; j <= Math.sqrt(i); j++) {
                 if (i % j == 0) {
