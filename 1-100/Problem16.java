dec7a5c333c3557c8b2399d5c875a168f9f901f0
Blake1412 <blakeryan30@gmail.com>
2021-12-21 19:20:52 +0000

    Create Problem16.java

---
+++ 1-100/Problem16.java
@@ -0,0 +1,13 @@
+import java.math.*;
+
+public class Problem16 {
+    public static int answer() {
+        int sum = 0;
+        BigInteger number = BigInteger.valueOf(2);
+        number = number.pow(1000);
+        for (int i = 0; i < number.toString().length(); i++) {
+            sum += Character.getNumericValue(number.toString().charAt(i));
+        }
+        return sum;
+    }
+}
