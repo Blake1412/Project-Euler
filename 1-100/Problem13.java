59ea9c120a478c5d605212a08336f80110e9b43f
Blake1412 <blakeryan30@gmail.com>
2021-12-21 16:55:19 +0000

    Create Problem13.java

---
+++ 1-100/Problem13.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Problem13 {
+    public static String answer() throws FileNotFoundException {
+        BigInteger sum = BigInteger.valueOf(0);
+        Scanner scan = new Scanner(new File("data.txt"));
+        while (scan.hasNext()) {
+            BigInteger number = new BigInteger(scan.nextLine());
+            sum = sum.add(number);
+        }
+        return sum.toString().substring(0, 10);
+    }
+}
