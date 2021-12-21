39f9b275ae121116a84d57e37c1b4a46b0a9de90
Blake1412 <blakeryan30@gmail.com>
2021-12-21 17:47:06 +0000

    Create Problem14.java

---
+++ 1-100/Problem14.java
@@ -0,0 +1,23 @@
+public class Problem14 {
+    public static long answer() {
+        long number = 0;
+        int largestTerm = 0;
+        for (int i = 1; i < 1000000; i++) {
+            int count = 1;
+            long n = i;
+            while (n > 1) {
+                if (n % 2 == 0) {
+                    n /= 2;
+                } else {
+                    n = 3 * n + 1;
+                }
+                count++;
+            }
+            if (count > largestTerm) {
+                largestTerm = count;
+                number = i;
+            }
+        }
+        return number;
+    }
+}
