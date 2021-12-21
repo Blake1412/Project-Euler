f5add75067011a065510030692aa29b10d374ccf
Blake1412 <blakeryan30@gmail.com>
2021-12-21 13:59:41 +0000

    Create Problem10.java

---
+++ 1-100/Problem10.java
@@ -0,0 +1,18 @@
+public class Problem10 {
+    public static long answer() {
+        long sum = 2;
+        for (int i = 3; i < 2000000; i ++) {
+            int flag = 0;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    flag++;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
