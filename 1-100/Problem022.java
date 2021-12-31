9f2743fb9c65b6f9833ef7ad288dc3f0cd579ebf
Blake1412 <blakeryan30@gmail.com>
2021-12-31 11:03:32 +0000

    Name formatting

---
+++ 1-100/Problem022.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Problem022 {
+
+    public static long answer() throws FileNotFoundException {
+        List<String> names = new ArrayList<>();
+        Scanner scan = new Scanner(new File("data.txt"));
+        while (scan.hasNext()) {
+            for (String s : scan.next().split(",")) {
+                names.add(s.substring(1, s.length() - 1));
+            }
+        }
+        Collections.sort(names);
+
+        long sum = 0;
+        for (int i = 0; i < names.size(); i++) {
+            int nameSum = 0;
+            for (int j = 0; j < names.get(i).length(); j++) {
+                nameSum += (names.get(i).charAt(j) - 64);
+            }
+            sum += nameSum * (i + 1);
+        }
+        return sum;
+    }
+}
